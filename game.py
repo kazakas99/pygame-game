aca30495a3a1dfcc0e32720693f1d5877e290534
Edvin <edvinas.katenel@gmail.com>
2026-03-01 17:42:38 +0200

    Fix snail movement and rendering

--- game.py
+++ game.py
@@ -13,12 +13,14 @@ background = pygame.image.load('background/background.jpg').convert()
 background = pygame.transform.scale(background, screen.get_size())
 
 player = pygame.image.load('characters/character.png').convert_alpha()
-player = pygame.transform.scale(player, (int(player.get_width() * 0.25), int(player.get_height() * 0.25)))
-player_rect = player.get_rect(topleft = (0, 203))
+player = pygame.transform.scale(
+    player, (int(player.get_width() * 0.25), int(player.get_height() * 0.25)))
+player_rect = player.get_rect(topleft=(0, 203))
 
 snail = pygame.image.load('characters/snail.png').convert_alpha()
-snail = pygame.transform.scale(snail, (int(snail.get_width() * 0.45), int(snail.get_height() * 0.45)))
-snail_rect = snail.get_rect(topright = (800, 210))
+snail = pygame.transform.scale(
+    snail, (int(snail.get_width() * 0.45), int(snail.get_height() * 0.45)))
+snail_rect = snail.get_rect(topright=(800, 210))
 
 while running:
     for event in pygame.event.get():
@@ -29,15 +31,15 @@ while running:
 
     # RENDER YOUR GAME HERE
     screen.blit(background, (0, 0))
-    
+
     screen.blit(text, (300, 50))
 
     screen.blit(player, player_rect)
 
     snail_rect.x -= 4
-    if snail_rect.right >= 0:
+    if snail_rect.right <= 0:
         snail_rect.left = 800
-    screen.blit(snail, (snail_rect))
+    screen.blit(snail, snail_rect)
 
     pygame.display.update()
     clock.tick(60)
